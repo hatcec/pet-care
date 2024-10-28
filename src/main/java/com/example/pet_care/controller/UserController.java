d0f5f5bb45beac0402d88b1a9da4f7c3d6fbbd91
pc <htccnbz06@gmail.com>
2024-10-29 00:35:02 +0300

    users crud

--- src/main/java/com/example/pet_care/controller/UserController.java
+++ src/main/java/com/example/pet_care/controller/UserController.java
@@ -3,35 +3,95 @@ package com.example.pet_care.controller;
 import com.example.pet_care.dto.EntityConverter;
 import com.example.pet_care.dto.UserDto;
 import com.example.pet_care.entities.User;
+import com.example.pet_care.exception.ResourceNotFoundException;
 import com.example.pet_care.exception.UserAllReadyExistsException;
 import com.example.pet_care.request.RegistrationRequest;
+import com.example.pet_care.request.UserUpdateRequest;
 import com.example.pet_care.response.ApiResponse;
 import com.example.pet_care.service.user.UserService;
+import com.example.pet_care.utils.FeedBackMessage;
+import com.example.pet_care.utils.UrlMapping;
+import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping(UrlMapping.USERS)
 @RestController
 public class UserController {
 
     private final UserService userService;
     private final EntityConverter<User, UserDto> entityConverter;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse> add(@RequestBody RegistrationRequest registrationRequest)
+    @PostMapping(UrlMapping.REGISTER_USER)
+    public ResponseEntity<ApiResponse> register(@RequestBody RegistrationRequest registrationRequest)
     {
       try{
-          User theUser=userService.add(registrationRequest);
+          User theUser=userService.register(registrationRequest);
           UserDto registeredUser=entityConverter.mapEntityToDto(theUser, UserDto.class);
-          return ResponseEntity.ok(new ApiResponse("USer registered successfully " , registeredUser));
+          return ResponseEntity.ok(new ApiResponse(FeedBackMessage.SUCCESS , registeredUser));
       }catch (UserAllReadyExistsException e){
-        return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
+        return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+      }
+      catch (Exception e){
+          return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
       }
 
     }
+
+    @PutMapping(UrlMapping.UPDATE_USER)
+    public ResponseEntity<ApiResponse> update(@PathVariable Long userId,@RequestBody UserUpdateRequest userUpdateRequest){
+        try {
+            User theUser=userService.updateUser(userId, userUpdateRequest);
+            UserDto updatedUser=entityConverter.mapEntityToDto(theUser, UserDto.class);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.UPDATE_SUCCESS, updatedUser));
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+
+        }
+        catch (Exception e){
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping(UrlMapping.GET_USER_BY_ID)
+    public ResponseEntity<ApiResponse> findById(@PathVariable Long userId){
+        try {
+            User user=userService.findById(userId);
+            UserDto theUser=entityConverter.mapEntityToDto(user, UserDto.class);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.FOUND, theUser));
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+
+        }
+        catch (Exception e){
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @DeleteMapping(UrlMapping.DELETE_USER_BY_ID)
+    public ResponseEntity<ApiResponse> deleteById(@PathVariable Long userId){
+        try {
+            userService.delete(userId);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.DELETE_SUCCESS, null));
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e){
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping(UrlMapping.GET_ALL_USERS)
+    public ResponseEntity<ApiResponse> getAllUsers(){
+        List<UserDto>  theUsers=userService.getAllUsers();
+        return ResponseEntity.ok(new ApiResponse(FeedBackMessage.SUCCESS, theUsers));
+    }
+
 }
--- src/main/java/com/example/pet_care/service/user/UserService.java
+++ src/main/java/com/example/pet_care/service/user/UserService.java
@@ -1,19 +1,62 @@
 package com.example.pet_care.service.user;
 
+import com.example.pet_care.dto.EntityConverter;
+import com.example.pet_care.dto.UserDto;
 import com.example.pet_care.entities.User;
+import com.example.pet_care.exception.ResourceNotFoundException;
 import com.example.pet_care.factory.UserFactory;
 import com.example.pet_care.repository.UserRepository;
 import com.example.pet_care.request.RegistrationRequest;
+import com.example.pet_care.request.UserUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements IUserService {
     private final UserRepository userRepository;
     private final UserFactory userFactory;
-    public User add(RegistrationRequest registrationRequest) {
+    private final EntityConverter<User, UserDto> entityConverter;
+    @Override
+    public User register(RegistrationRequest registrationRequest) {
 
        return userFactory.createUser(registrationRequest);
     }
+
+    @Override
+    public User updateUser(Long userId, UserUpdateRequest userUpdateRequest){
+        User user=findById(userId);
+        user.setFirstName(userUpdateRequest.getFirstName());
+        user.setLastName(userUpdateRequest.getLastName());
+        user.setGender(userUpdateRequest.getGender());
+        user.setPassword(userUpdateRequest.getPhoneNumber());
+        user.setPhoneNumber(userUpdateRequest.getPhoneNumber());
+        user.setSpecialization(userUpdateRequest.getSpecialization());
+
+
+        return userRepository.save(user);
+    }
+    @Override
+    public void delete(Long userId){
+        userRepository.findById(userId).ifPresentOrElse(userRepository::delete, ()->{
+            throw new ResourceNotFoundException("User not found");
+        });
+    }
+    @Override
+    public User findById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("USer not found"));
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users
+                .stream()
+                .map(user -> entityConverter.mapEntityToDto(user, UserDto.class))
+                .collect(Collectors.toList());
+    }
+
 }
